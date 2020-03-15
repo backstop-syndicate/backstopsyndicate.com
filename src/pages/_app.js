a0f1ed77029c7fddef3cf45d21bf857a4c016b14
Mitchell DeMarco <Neablis121@gmail.com>
2020-03-15 14:43:31 -0700

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,14 @@ import '../styles.css'
 import React from "react";
 import AppHeader from "../components/AppHeader";
 import Footer from "../components/Footer";
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
     return <div>
+        <Head>
+            <link key={"shortcut-favicon"} rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+            <link key={"favicon"} rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </Head>
         <AppHeader />
         <Component {...pageProps} />
         <Footer />
